b57f202ea077531b8baa3c96e23dc5c36a3dab8f
timmy-ctrl <timmygithub@gmail.com>
2025-09-05 14:08:22 +0800

    f

--- app/Http/Controllers/main/api/Review/StoreController.php
+++ app/Http/Controllers/main/api/Review/StoreController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Main\api\Review;
 
 use App\Http\Controllers\Controller;
 
+
 class StoreController extends Controller
 {
-    //
+    public function __invoke()
+    {
+        return response()->json('Мы тут');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\Main\api\Book\IndexController;
 use App\Http\Controllers\Main\api\Book\ShowController;
+use App\Http\Controllers\Main\api\Review\StoreController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/books', IndexController::class);
 Route::get('/books/{book}', ShowController::class);
+Route::get('/books/{book}/reviews', StoreController::class);
